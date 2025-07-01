9ba3e0fc5a74d28f3b9f5031c98c9d3f17a9f841
Yannick Van kerckvoorde <vankerckvoordeyannick@gmail.com>
2025-07-01 10:15:42 +0200

    Added seeding

--- Test Ef/Models/LandContext.cs
+++ Test Ef/Models/LandContext.cs
@@ -22,12 +22,58 @@ public class LandContext : DbContext {
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder) {
-        modelBuilder.Entity<Land>().HasMany(l => l.Talen);
-        modelBuilder.Entity<Taal>().HasMany(t => t.Landen);
-        modelBuilder.Entity<Stad>().HasOne(s => s.Land);
+        modelBuilder.Entity<Land>()
+            .HasMany(land => land.Talen)
+            .WithMany(taal => taal.Landen)
+            .UsingEntity(j => {
+                j.HasData(
+                    new { LandenLandCode = "BEL", TalenTaalCode = "de" },
+                    new { LandenLandCode = "BEL", TalenTaalCode = "fr" },
+                    new { LandenLandCode = "BEL", TalenTaalCode = "nl" },
+                    new { LandenLandCode = "DEU", TalenTaalCode = "de" },
+                    new { LandenLandCode = "FRA", TalenTaalCode = "fr" },
+                    new { LandenLandCode = "LUX", TalenTaalCode = "de" },
+                    new { LandenLandCode = "LUX", TalenTaalCode = "fr" },
+                    new { LandenLandCode = "LUX", TalenTaalCode = "lb" },
+                    new { LandenLandCode = "NLD", TalenTaalCode = "nl" }
+                );
+                j.ToTable("LandenTalen");
+            });
+        
+        modelBuilder.Entity<Stad>()
+            .HasOne(s => s.Land)
+            .WithMany(l => l.Steden)
+            .HasForeignKey(s => s.LandCode); 
 
         modelBuilder.Entity<Land>().HasData([
-            new Land { LandCode = "BEL", Naam = "Naam" },
+            new Land { LandCode = "BEL", Naam = "Belgie"},
+            new Land { LandCode = "DEU", Naam = "Duitsland" },
+            new Land { LandCode = "FRA", Naam = "Frankrijk" },
+            new Land { LandCode = "LUX", Naam = "Luxemburg" },
+            new Land { LandCode = "NLD", Naam = "Nederland" },
+        ]);
+        
+        modelBuilder.Entity<Taal>().HasData([
+            new Taal { TaalCode = "de", Naam = "Duits" },
+            new Taal { TaalCode = "fr", Naam = "Frans" },
+            new Taal { TaalCode = "lb", Naam = "Luxemburgs" },
+            new Taal { TaalCode = "nl", Naam = "Nederlands" },
+        ]);
+        
+        modelBuilder.Entity<Stad>().HasData([
+            new Stad { StadNummer = 1, Naam = "Brussel", LandCode = "BEL" },
+            new Stad { StadNummer = 2, Naam = "Antwerpen", LandCode = "BEL" },
+            new Stad { StadNummer = 3, Naam = "Luik", LandCode = "BEL" },
+            new Stad { StadNummer = 4, Naam = "Amsterdam", LandCode = "NLD" },
+            new Stad { StadNummer = 5, Naam = "Den Haag", LandCode = "NLD" },
+            new Stad { StadNummer = 6, Naam = "Rotterdam", LandCode = "NLD" },
+            new Stad { StadNummer = 7, Naam = "Berlijn", LandCode = "DEU" },
+            new Stad { StadNummer = 8, Naam = "Hamburg", LandCode = "DEU" },
+            new Stad { StadNummer = 9, Naam = "Munchen", LandCode = "DEU" },
+            new Stad { StadNummer = 10, Naam = "Luxemburg", LandCode = "LUX" },
+            new Stad { StadNummer = 11, Naam = "Parijs", LandCode = "FRA" },
+            new Stad { StadNummer = 12, Naam = "Marseille", LandCode = "FRA" },
+            new Stad { StadNummer = 13, Naam = "Lyon", LandCode = "FRA" }
         ]);
     }
 }
