1f5bea37bc46d83e0f09780769e8b91ba37f053e
Yannick Van kerckvoorde <vankerckvoordeyannick@gmail.com>
2025-07-01 09:19:51 +0200

    Migrations

---
+++ Test Ef/Migrations/20250701071806_Initial.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Test_Ef.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Landen",
+                columns: table => new
+                {
+                    LandCode = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Naam = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Landen", x => x.LandCode);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Talen",
+                columns: table => new
+                {
+                    TaalCode = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Naam = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Talen", x => x.TaalCode);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Steden",
+                columns: table => new
+                {
+                    StadNr = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Naam = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    LandCode = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Steden", x => x.StadNr);
+                    table.ForeignKey(
+                        name: "FK_Steden_Landen_LandCode",
+                        column: x => x.LandCode,
+                        principalTable: "Landen",
+                        principalColumn: "LandCode",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LandTaal",
+                columns: table => new
+                {
+                    LandenLandCode = table.Column<int>(type: "int", nullable: false),
+                    TalenTaalCode = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LandTaal", x => new { x.LandenLandCode, x.TalenTaalCode });
+                    table.ForeignKey(
+                        name: "FK_LandTaal_Landen_LandenLandCode",
+                        column: x => x.LandenLandCode,
+                        principalTable: "Landen",
+                        principalColumn: "LandCode",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_LandTaal_Talen_TalenTaalCode",
+                        column: x => x.TalenTaalCode,
+                        principalTable: "Talen",
+                        principalColumn: "TaalCode",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LandTaal_TalenTaalCode",
+                table: "LandTaal",
+                column: "TalenTaalCode");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Steden_LandCode",
+                table: "Steden",
+                column: "LandCode");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LandTaal");
+
+            migrationBuilder.DropTable(
+                name: "Steden");
+
+            migrationBuilder.DropTable(
+                name: "Talen");
+
+            migrationBuilder.DropTable(
+                name: "Landen");
+        }
+    }
+}
