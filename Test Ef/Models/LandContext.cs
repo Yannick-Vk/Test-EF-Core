d8ef1ed604674b6e3d27d58e3aadb5c4f3b41fc9
Yannick Van kerckvoorde <vankerckvoordeyannick@gmail.com>
2025-07-01 09:19:44 +0200

    Added config and onModelCreation

--- Test Ef/Models/LandContext.cs
+++ Test Ef/Models/LandContext.cs
@@ -1,10 +1,32 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace Test_Ef.Models;
 
 public class LandContext : DbContext {
+    public static IConfigurationRoot config;
+    
     public DbSet<Land> Landen { get; set; }
     public DbSet<Stad> Steden { get; set; }
     public DbSet<Taal> Talen  { get; set; }
     
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+        config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetParent(AppContext.BaseDirectory)?.FullName)
+            .AddJsonFile("appsettings.json", false).Build();
+        var connectionString =
+            config.GetConnectionString("Landen");
+        if (connectionString != null)
+            optionsBuilder.UseSqlServer(connectionString,
+                options => options.MaxBatchSize(150));
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        modelBuilder.Entity<Land>()
+            .HasMany(l => l.Talen)
+            ;
+        modelBuilder.Entity<Stad>().HasOne(s => s.Land);
+        
+        modelBuilder.Entity<Taal>().HasMany(t => t.Landen);
+    }
 }
